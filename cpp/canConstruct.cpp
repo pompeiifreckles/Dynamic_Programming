fe665778ca4ebcb38a5730919d269f52a3d36302
pompeiifreckles <pompeiifreckles@tux>
2021-05-14 09:46:49 +0530

    memoized canConstruct

--- cpp/canConstruct.cpp
+++ cpp/canConstruct.cpp
@@ -6,22 +6,40 @@
 // canConstruct(skateboard, [ab, abc, cd, def, abcd]) -> false   
 
 #include <iostream>
+#include <functional>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 bool canConstruct(string target, const vector<string>& wordBank) {
-	if (target == "") return true;
+	function<bool(string, const vector<string>&)> impl;
+	impl = [memo=map<string, bool>(), &impl] (string target, const vector<string>& wordBank) mutable -> bool {
 
-	for (auto word : wordBank) {
-		int size = word.size();
-		if (target.compare(0, size, word) == 0) {
-			bool result = canConstruct(target.substr(0, target.size() - word.size()), wordBank);
-			if (result) return true;
+		map<string, bool>::iterator it;
+		if ((it = memo.find(target)) != memo.end()) return it->second;
+		if (target == "") return true;
+
+		for (auto word : wordBank) {
+			int size = word.size();
+			if (target.compare(0, size, word) == 0) {
+				bool result = impl(target.substr(0, target.size() - word.size()), wordBank);
+				if (result) {
+					memo[target] = true;
+					return true;
+				}
+			}
 		}
-	}
 
-	return false;
+		memo[target] = false;
+		return false;
+	};
+
+	return impl(target, wordBank);
+}
+
+bool canConstruct(string target, const vector<string>&& wordBank) {
+	return canConstruct(target, wordBank);
 }
 
 int main() {
